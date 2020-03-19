55d4ae4636e0b153e4c626772e46930de98c14ad
Janssen PILLEBOUT <janssen.pillebout@gmail.com>
2020-03-19 11:43:40 +0100

    Modification sur le fichier wp-config.php : - Limitations des revisions à 5 - Paramètre d'édition du code sur le panel admin désactivé - Mode debug activé

--- childguard-wp/wp-config.php
+++ childguard-wp/wp-config.php
@@ -36,6 +36,8 @@ define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+define('WP_POST_REVISIONS', 5 );
+define( 'DISALLOW_FILE_EDIT', true );
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -77,7 +79,7 @@ $table_prefix = 'chi_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* That's all, stop editing! Happy publishing. */
 
