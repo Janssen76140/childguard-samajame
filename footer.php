ec1527c191ba52261bf9a2e82a614109d24fb083
Mathis <houletmathis@gmail.com>
2020-03-19 16:35:01 +0100

    footer terminé

--- footer.php
+++ footer.php
@@ -1,5 +1,15 @@
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="asset/css/style.css">
+
 <footer>
-    `
+    <nav class="navbarFooter">
+        <ul>
+            <li><a href="#" class="lienf home">Accueil</a></li>
+            <li><a href="#" class="lienf contact">Contact</a></li>
+            <li><a href="#" class="lienf conditions">CGU</a></li>
+            <li><a href="#" class="lienf mentions">Mentions Légales</a></li>
+        </ul>
+    </nav>
 </footer>
 
 </body>
