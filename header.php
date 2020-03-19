6fb4bd2c2c7a8397b6b2f26b9fb6088c7a4934f3
Mathis <houletmathis@gmail.com>
2020-03-19 15:16:57 +0100

    header sur nouvelle branche

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+
+<body>
+
+<header>
+
+    <div>
+        <a href="#"><img class="logo" src="asset/img/logosamajame_rd_contour.svg" alt="Logo ChildGuard"/></a>
+        <nav class="navbarHeader">
+            <ul>
+                <li><a href="#" class="lien connexionHeader">Connexion</a></li>
+                <li><a href="#" class="lien inscriptionHeader">Inscription</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</header>
