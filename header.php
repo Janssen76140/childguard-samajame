61d1f31ab2bb218824c2f5c1202bb90160667321
Mathis <houletmathis@gmail.com>
2020-03-19 12:52:38 +0100

    correction erreur de dossier pendant récupértion git

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="asset/css/style.css">
+</head>
+
+<body>
+
+    <header>
+
+        <div>
+            <img class="logo" src="asset/img/logo.png" alt="Logo ChildGuard">
+                <nav class="navbarHeader">
+                    <ul>
+                        <li><a href="#" class="link connectHeader">Connexion</a></li>
+                        <li><a href="#" class="link signUpHeader">Inscription</a></li>
+                    </ul>
+                </nav>
+        </div>
+
+    </header>
