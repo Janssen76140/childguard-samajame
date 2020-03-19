273221c74c1ff6f246d09a2cd0d65477ecf20dca
Mathis <houletmathis@gmail.com>
2020-03-19 13:16:32 +0100

    finitions du header

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
     <header>
 
         <div>
-            <img class="logo" src="asset/img/logo.png" alt="Logo ChildGuard">
+            <a href="#"><img class="logo" src="asset/img/logo.png" alt="Logo ChildGuard"/></a>
                 <nav class="navbarHeader">
                     <ul>
                         <li><a href="#" class="lien connexionHeader">Connexion</a></li>
