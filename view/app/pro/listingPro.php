8bcaa13a2af5be82d93769902662d89670e7f701
SamuelLacaille <samuel76560@outlook.fr>
2020-04-01 12:21:50 +0200

    ajout de la page listingPro qui permet de visualiser des infos d'une crèche dans un tableau

---
+++ view/app/pro/listingPro.php
@@ -0,0 +1,32 @@
+<?php ?>
+
+
+
+<table>
+    <thead>
+    <tr>
+        <th>Nom</th>
+        <th>Prix</th>
+        <th>Email</th>
+        <th>Heure d'ouverture </th>
+        <th>Heure de fermeture</th>
+        <th>Adresse</th>
+        <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach($pros as $pro) { ?>
+        <tr>
+            <td><?= $pro->nom; ?></td>
+            <td><?= $pro->prix; ?> €/heure</td>
+            <td><?= $pro->email; ?></td>
+            <td><?= $pro->heureDebut; ?></td>
+            <td><?= $pro->heureFin; ?></td>
+            <td><?= $pro->adresse; ?></td>
+            <td>
+                <a href="<?= $view->path('detailPro',array($pro->id)); ?>">Voir</a>
+            </td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
