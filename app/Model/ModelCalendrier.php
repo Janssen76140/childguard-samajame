8e184cbea05c63f0457a2db7c47b8bc3e1b2632a
SamuelLacaille <samuel76560@outlook.fr>
2020-04-06 14:29:26 +0200

    listing des enfants de l'utilisateur à glisser/deposer sur le planning de la crèche

--- app/Model/ModelCalendrier.php
+++ app/Model/ModelCalendrier.php
@@ -7,7 +7,7 @@ use App\Weblitzer\Model;
 class ModelCalendrier extends Model
 
 {
-   // private $prenomEnfant;
+
     protected static $table = 'chi_enfant_has_chi_pro';
 
 
@@ -19,15 +19,18 @@ class ModelCalendrier extends Model
 
 
 
-    public static function listingEnfantDuParent($prenomEnfants)
+    public static function listingEnfantDuParent($chi_user_id_user)
     {
-      /*  global $pdo;
+    /*   global $pdo;
 
         $sql = "SELECT prenom FROM chi_enfant LEFT JOIN chi_enfant_has_chi_user ON chi_enfant.id_enfant = chi_enfant_has_chi_user.chi_enfant_id_enfant WHERE chi_user_id_user = chi_enfant.id_parent";
         $query = $pdo->prepare($sql);
         $query->execute();
         $prenomEnfants = $query->fetchAll();
-      */
+        return ($prenomEnfants);
+*/
+        return App::getDatabase()->prepare("SELECT prenom FROM chi_enfant LEFT JOIN chi_enfant_has_chi_user ON chi_enfant.id_enfant = chi_enfant_has_chi_user.chi_enfant_id_enfant  WHERE  chi_user_id_user = chi_enfant.id_parent",[$chi_user_id_user],get_called_class(),false);
+
     }
 
 
