d883371b450261febca065fe0b2e7d4761f6d84c
SamuelLacaille <samuel76560@outlook.fr>
2020-04-08 11:11:08 +0200

    ajout de <div class="clear"></div> pour ne pas afficher la carte dans une partie du header

--- view/app/carte/carte.php
+++ view/app/carte/carte.php
@@ -1,14 +1,15 @@
 <?php ?>
 
-
-<div id='map' style='width: 90%; height: 1000px;padding-top: 250px;padding-left: 5%; padding-right: 5%;'></div>
+<div class="clear"></div>
+<div id='map' style='width: 100%; height: 600px;margin-top: 150px;margin-bottom: 50px;'></div>
+<div class="clear"></div>
 <script>
     mapboxgl.accessToken = 'pk.eyJ1IjoiczRzNG0iLCJhIjoiY2s4cXp3OXg1MDRlMDNsdGxweHNyaGh0ZiJ9.cVEr5FCPeU4qpNREKvdIRA';
     var map = new mapboxgl.Map({
         container: 'map', // container id
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-96, 37.8], // starting position
-        zoom: 3 // starting zoom
+        zoom: 4 // starting zoom
     });
 
     // Add geolocate control to the map.
